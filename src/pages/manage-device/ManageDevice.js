73a3ca276327c316bdc88262db3d939dcade6fde
tuanduongg <tuanmixmusic@gmail.com>
2024-05-19 06:27:51 +0700

    commmit 19-5

--- src/pages/manage-device/ManageDevice.js
+++ src/pages/manage-device/ManageDevice.js
@@ -16,18 +16,19 @@ import {
   Switch,
   Image,
   notification,
-  Checkbox
+  Card
 } from 'antd';
 const { Search } = Input;
 import { formattingVND, isMobile, truncateString } from 'utils/helper';
 import ForbidenPage from 'components/403/ForbidenPage';
 import { RouterAPI } from 'utils/routerAPI';
 import restApi from 'utils/restAPI';
-import { PlusOutlined, DownOutlined, FilterOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import { PlusOutlined, DownOutlined, FilterOutlined, EditOutlined, DeleteOutlined, FundProjectionScreenOutlined } from '@ant-design/icons';
 import Loading from 'components/Loading';
 import MainCard from 'components/MainCard';
 import { useTranslation } from 'react-i18next';
 import config from 'config';
+import './manage_device.css';
 
 const { Title, Link } = Typography;
 
@@ -65,7 +66,7 @@ const ManageDevice = () => {
         <>
           <Row style={{ display: 'flex', alignItems: 'center' }} gutter={8}>
             <Col xs={24} sm={20} lg={20}>
-              <Link onClick={() => {}}>{data?.productName}</Link>
+              <Link onClick={() => { }}>{data?.productName}</Link>
             </Col>
           </Row>
         </>
@@ -128,7 +129,7 @@ const ManageDevice = () => {
         <>
           {' '}
           <Switch
-            onClick={() => {}}
+            onClick={() => { }}
             size="small"
             checkedChildren="Show"
             unCheckedChildren="Hide"
@@ -167,7 +168,7 @@ const ManageDevice = () => {
       danger: true
     }
   ];
-  const handleMenuClick = () => {};
+  const handleMenuClick = () => { };
   const menuProps = {
     items: ITEMS,
     onClick: handleMenuClick
@@ -191,6 +192,17 @@ const ManageDevice = () => {
   //   if (!role?.IS_READ) {
   //     return <ForbidenPage />;
   //   }
+
+  const LIST_CARD = [
+    {
+      xs: 1,
+      sm: 1,
+      md: 1,
+      title: 'Tổng',
+      value: '1000',
+      color: '#3d0789'
+    }
+  ]
   const handleChange = (value) => {
     console.log(`selected ${value}`);
   };
@@ -198,6 +210,117 @@ const ManageDevice = () => {
     <>
       <Loading loading={loading} />
       {contextHolder}
+      <Row style={{ marginBottom: '5px', alignItems: 'center' }} gutter={[8, 8]}>
+        <Col xs={12} sm={8} md={4}>
+          <Card
+            style={{
+              width: '100%',
+              height: '100px'
+            }}
+            className="card_statistic_device"
+          >
+            <Row>
+              <Col style={{ display: 'flex', alignItems: 'center' }} xs={5}>
+
+                <FundProjectionScreenOutlined style={{ fontSize: '50px' }} />
+              </Col>
+              <Col xs={19}>
+                <div style={{ fontWeight: 'bold', fontSize: '25px', textAlign: 'center' }}>1000</div>
+                <div style={{ fontWeight: '500', fontSize: '13px', textAlign: 'center' }}>TỔNG</div>
+                <div></div>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+        <Col xs={12} sm={8} md={5}>
+          <Card
+            style={{
+              width: '100%',
+              height: '100px'
+            }}
+            className="card_statistic_device"
+          >
+            <Row>
+              <Col style={{ display: 'flex', alignItems: 'center' }} xs={5}>
+                {/* <Button type="link" size="large" icon={}></Button> */}
+
+                <FundProjectionScreenOutlined style={{ fontSize: '50px' }} />
+              </Col>
+              <Col xs={14}>
+                <div style={{ fontWeight: 'bold', fontSize: '25px', textAlign: 'center' }}>1000</div>
+                <div style={{ fontWeight: '500', fontSize: '13px', textAlign: 'center' }}>ĐANG SỬ DỤNG</div>
+                <div></div>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+        <Col xs={12} sm={8} md={5}>
+          <Card
+            style={{
+              width: '100%',
+              height: '100px'
+            }}
+            className="card_statistic_device"
+          >
+            <Row>
+              <Col style={{ display: 'flex', alignItems: 'center' }} xs={5}>
+                {/* <Button type="link" size="large" icon={}></Button> */}
+
+                <FundProjectionScreenOutlined style={{ fontSize: '50px' }} />
+              </Col>
+              <Col xs={19}>
+                <div style={{ fontWeight: 'bold', fontSize: '25px', textAlign: 'center' }}>1000</div>
+                <div style={{ fontWeight: '500', fontSize: '13px', textAlign: 'center' }}>ĐÃ HỎNG</div>
+                <div></div>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+        <Col xs={12} sm={8} md={5}>
+          <Card
+            style={{
+              width: '100%',
+              height: '100px'
+            }}
+            className="card_statistic_device"
+          >
+            <Row>
+              <Col style={{ display: 'flex', alignItems: 'center' }} xs={5}>
+                {/* <Button type="link" size="large" icon={}></Button> */}
+
+                <FundProjectionScreenOutlined style={{ fontSize: '50px' }} />
+              </Col>
+              <Col xs={19}>
+                <div style={{ fontWeight: 'bold', fontSize: '25px', textAlign: 'center' }}>1000</div>
+                <div style={{ fontWeight: '500', fontSize: '13px', textAlign: 'center' }}>RẢNH</div>
+                <div></div>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+        <Col xs={24} sm={5}>
+          <Card
+            style={{
+              width: '100%',
+              height: '100px'
+            }}
+            className="card_statistic_device"
+          >
+            <Row>
+              <Col style={{ display: 'flex', alignItems: 'center' }} xs={5}>
+                {/* <Button type="link" size="large" icon={}></Button> */}
+
+                <FundProjectionScreenOutlined style={{ fontSize: '50px' }} />
+              </Col>
+              <Col xs={19}>
+                <div style={{ fontWeight: 'bold', fontSize: '25px', textAlign: 'center' }}>1000</div>
+                <div style={{ fontWeight: '500', fontSize: '13px', textAlign: 'center' }}>HẾT BẢO HÀNH 04/2024</div>
+                <div></div>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
       <MainCard contentSX={{ p: isMobile() ? 0.5 : 2, minHeight: '83vh' }}>
         <Row gutter={8}>
           <Col xs={0} sm={5} md={3}>
@@ -280,16 +403,16 @@ const ManageDevice = () => {
             />
           </Col>
           <Col xs={0} sm={5} md={6}>
-            <Search style={{ maxWidth: '300px' }} placeholder="Tên thiết bị..." allowClear enterButton="Search" onSearch={() => {}} />
+            <Search style={{ maxWidth: '300px' }} placeholder="Tên thiết bị..." allowClear enterButton="Search" onSearch={() => { }} />
           </Col>
           <Col xs={24} sm={4} md={9}>
             <div style={{ display: 'flex', justifyContent: 'end', alignItems: 'center' }}>
               {isMobile() && (
-                <Button onClick={() => {}} style={{ marginRight: '5px' }} icon={<FilterOutlined />}>
+                <Button onClick={() => { }} style={{ marginRight: '5px' }} icon={<FilterOutlined />}>
                   Bộ lọc
                 </Button>
               )}
-              <Button onClick={() => {}} style={{ marginRight: '10px' }} icon={<PlusOutlined />} type="primary">
+              <Button onClick={() => { }} style={{ marginRight: '10px' }} icon={<PlusOutlined />} type="primary">
                 {t('btn_new')}
               </Button>
               <Dropdown menu={menuProps}>
@@ -313,27 +436,27 @@ const ManageDevice = () => {
               scroll={
                 isMobile()
                   ? {
-                      x: '100vh',
-                      y: '65vh'
-                    }
+                    x: '100vh',
+                    y: '65vh'
+                  }
                   : { x: null, y: 'calc(100vh - 230px)' }
               }
               columns={columns}
               dataSource={[]}
-              //   pagination={{
-              //     current: page,
-              //     pageSize: rowsPerPage,
-              //     showSizeChanger: true,
-              //     pageSizeOptions: config.sizePageOption,
-              //     total: total,
-              //     showTotal: (total, range) => `${range[0]}-${range[1]}/${total}`,
-              //     responsive: true,
-              //     onChange: (page, pageSize) => {
-              //       setSelectedRowKeys([]);
-              //       setPage(page);
-              //       setRowsPerPage(pageSize);
-              //     }
-              //   }}
+            //   pagination={{
+            //     current: page,
+            //     pageSize: rowsPerPage,
+            //     showSizeChanger: true,
+            //     pageSizeOptions: config.sizePageOption,
+            //     total: total,
+            //     showTotal: (total, range) => `${range[0]}-${range[1]}/${total}`,
+            //     responsive: true,
+            //     onChange: (page, pageSize) => {
+            //       setSelectedRowKeys([]);
+            //       setPage(page);
+            //       setRowsPerPage(pageSize);
+            //     }
+            //   }}
             ></Table>
           </Col>
         </Row>
